a0c37b5accfdfa910a854864bd6c60ecd6decfca
Werner LEMBERG <wl@gnu.org>
2013-01-07 08:41:22 +0000

    Revert previous change.

--- src/preproc/eqn/lex.cpp
+++ src/preproc/eqn/lex.cpp
@@ -1071,17 +1071,13 @@ void do_delim()
   else {
     if (c == 'o' && d == 'f' && peek_char() == 'f') {
       (void)get_char();
-      if (start_delim)
-	start_delim_saved = start_delim;
-      if (end_delim)
-	end_delim_saved = end_delim;
+      start_delim_saved = start_delim;
+      end_delim_saved = end_delim;
       start_delim = end_delim = '\0';
     }
     else if (c == 'o' && d == 'n' && !compatible_flag) {
-      if (start_delim_saved)
-	start_delim = start_delim_saved;
-      if (end_delim_saved)
-	end_delim = end_delim_saved;
+      start_delim = start_delim_saved;
+      end_delim = end_delim_saved;
     }
     else {
       start_delim = c;
