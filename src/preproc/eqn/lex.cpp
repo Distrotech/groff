f04340a44502ff22c961667caa4b313478b42077
Bertrand Garrigues <bertrand.garrigues@laposte.net>
2014-08-21 23:07:00 +0200

    Build all binaries in src/preproc and build arch/misc/shdeps.sed.
    * src/preproc: Add 1 .am file per subdirectory. Use `AM_YFLAGS' to
      pass option to yacc/flex.

--- src/preproc/eqn/lex.cpp
+++ src/preproc/eqn/lex.cpp
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 #include "eqn.h"
-#include "eqn_tab.h"
+#include "eqn.hpp"
 #include "stringclass.h"
 #include "ptable.h"
 
