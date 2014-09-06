aee96c8625a239acd6bfb36dd0e50409e3a41872
Bertrand Garrigues <bertrand.garrigues@laposte.net>
2014-09-06 14:37:32 +0200

    Add contrib/gdiffmk, plug gdiffmk test script to `make check'.
    Fix some comments in TESTS.

---
+++ contrib/gdiffmk/tests/gdiffmk_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Execute runtests.sh in the builddir 
+
+mkdir -p ${abs_top_builddir}/contrib/gdiffmk/tests
+cd ${abs_top_builddir}/contrib/gdiffmk/tests
+${abs_top_srcdir}/contrib/gdiffmk/tests/runtests.sh run
