36c3dfe23d8ca2af632020550323db01c355fc70
Bernd Warken <groff-bernd.warken-72@web.de>
2014-09-17 16:01:38 +0200

    src/devices/grolj4/*: Improve the license information.

--- src/devices/grolj4/lj4.cpp
+++ src/devices/grolj4/lj4.cpp
@@ -1,6 +1,5 @@
 // -*- C++ -*-
-/* Copyright (C) 1994, 2000, 2001, 2002, 2003, 2004, 2006, 2009
-   Free Software Foundation, Inc.
+/* Copyright (C) 1994-2014  Free Software Foundation, Inc.
      Written by James Clark (jjc@jclark.com)
 
 This file is part of groff.
