e375837773f99ea0de14ebebe5fb69e22b39fb28
Bernd Warken <groff-bernd.warken-72@web.de>
2014-09-18 01:24:39 +0200

    src/preproc/preconv/*: Improve the license information.

--- src/preproc/preconv/preconv.cpp
+++ src/preproc/preconv/preconv.cpp
@@ -1,6 +1,5 @@
 // -*- C++ -*-
-/* Copyright (C) 2005, 2006, 2008, 2009
-   Free Software Foundation, Inc.
+/* Copyright (C) 2005-2014  Free Software Foundation, Inc.
      Written by Werner Lemberg (wl@gnu.org)
 
 This file is part of groff.
