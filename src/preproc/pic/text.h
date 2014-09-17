11eaa8d62e4a1183823a20bd9b51864ceda636ff
Bernd Warken <groff-bernd.warken-72@web.de>
2014-09-18 01:03:41 +0200

    src/preproc/pic/*: Improve the license information.

--- src/preproc/pic/text.h
+++ src/preproc/pic/text.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-/* Copyright (C) 1989, 2009 Free Software Foundation, Inc.
+/* Copyright (C) 1989-2014  Free Software Foundation, Inc.
      Written by James Clark (jjc@jclark.com)
 
 This file is part of groff.
