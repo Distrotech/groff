3730968ed5702700d170300f9dd8a3dd553973ff
Bernd Warken <groff-bernd.warken-72@web.de>
2014-09-18 02:14:00 +0200

    src/preproc/tbl/*: Improve the license information.

--- src/preproc/tbl/main.cpp
+++ src/preproc/tbl/main.cpp
@@ -1,7 +1,5 @@
 // -*- C++ -*-
-/* Copyright (C) 1989, 1990, 1991, 1992, 2000, 2001, 2002, 2003, 2004, 2005,
-                 2007, 2008, 2009, 2010
-   Free Software Foundation, Inc.
+/* Copyright (C) 1989-2014  Free Software Foundation, Inc.
      Written by James Clark (jjc@jclark.com)
 
 This file is part of groff.
